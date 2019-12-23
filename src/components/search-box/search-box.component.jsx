f0b819853530f11efb2d5474dc1219bbdfef96a6
Tutan Das <tutan.essential@gmail.com>
2019-12-23 14:48:01 +0530

    initial commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({placeHolder, handleChange}) =>(
+    <input 
+        className="search"
+        type="search" 
+        placeholder={placeHolder} 
+        onChange={handleChange}></input>
+);
