caa040aae2d1ab8666b2835d839f1f5e2533003c
Tutan Das <tutan.essential@gmail.com>
2019-12-23 15:11:13 +0530

    Updates

---
+++ precache-manifest.246134e4ad74e67a492144d24717b275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5567334c55abd39275786374f7aeb0a4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9d2dae20eefcf7040cb2",
+    "url": "/monsters-rolodex/static/css/main.22740051.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "9d2dae20eefcf7040cb2",
+    "url": "/monsters-rolodex/static/js/main.60db50b7.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
